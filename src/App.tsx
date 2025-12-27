a7206abd21493fdcc884a67b8aff9a2a58a7a614
panosrodo <brandonroots@hotmail.gr>
2025-12-27 21:23:25 +0200

    feat: add routing
    feat(ui): add layout with header, footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import {BrowserRouter, Routes, Route} from "react-router";
+import Layout from "@/components/layout/Layout.tsx";
+import HomePage from "@/components/pages/HomePage.tsx";
 
 function App() {
-
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<Layout />}>
+                        <Route index element={<HomePage />}/>
+                        {/*<Route path="products">*/}
+                        {/*  <Route index element={<ProductListPage/>}/>*/}
+                        {/*  <Route path=":productId" element={<ProductPage/>}/>*/}
+                        {/*</Route>*/}
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
