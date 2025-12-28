b55162678799c0ad0f5f5f9007c5ee1469cf8eac
panosrodo <brandonroots@hotmail.gr>
2025-12-28 22:39:04 +0200

    feat: create product list page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import {BrowserRouter, Routes, Route} from "react-router";
 import Layout from "@/components/layout/Layout.tsx";
 import HomePage from "@/components/pages/HomePage.tsx";
+import ProductsPage from "@/components/pages/ProductsPage.tsx";
+import ProductPage from "@/components/pages/ProductPage.tsx";
 
 function App() {
     return (
@@ -9,10 +11,10 @@ function App() {
                 <Routes>
                     <Route element={<Layout />}>
                         <Route index element={<HomePage />}/>
-                        {/*<Route path="products">*/}
-                        {/*  <Route index element={<ProductListPage/>}/>*/}
-                        {/*  <Route path=":productId" element={<ProductPage/>}/>*/}
-                        {/*</Route>*/}
+                        <Route path="products">
+                            <Route index element={<ProductsPage/>}/>
+                            <Route path=":productId" element={<ProductPage/>}/>
+                        </Route>
                     </Route>
                 </Routes>
             </BrowserRouter>
